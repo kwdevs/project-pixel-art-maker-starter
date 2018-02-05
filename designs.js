271dd8239452d3cdac6f2d6c014d72e938160078
Kyle Morrison <morrison.devs@gmail.com>
2018-02-05 14:57:07 -0600

    feat: Updated DOM selection method for variables.

--- designs.js
+++ designs.js
@@ -1,9 +1,9 @@
 // Select color input
 // Select size input
 
-var colorInput = document.querySelector('#colorPicker');
-var canvasHeight = document.querySelector('#inputHeight');
-var canvasWidth = document.querySelector('#inputWidth');
+var colorInput = document.getElementById('#colorPicker');
+var canvasHeight = document.getElementById('#inputHeight');
+var canvasWidth = document.getElementById('#inputWidth');
 
 // When size is submitted by the user, call makeGrid()
 
