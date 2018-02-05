e22a90dfd961b6086522ceff449d4a383ed17862
Kyle Morrison <morrison.devs@gmail.com>
2018-02-05 17:06:23 -0600

    feat: event listener implemented but not functional.

--- designs.js
+++ designs.js
@@ -2,31 +2,39 @@
 // Select size input
 
 var colorInput = document.getElementById('colorPicker');
-var canvasHeight = document.getElementById('inputHeight');
-var canvasWidth = document.getElementById('inputWidth');
+var canvasHeight;
+var canvasWidth;
 var tableElement = document.getElementById('pixelCanvas');
+var GridSizeSubmitButton = document.querySelector('input[type=submit]');
 
 // When size is submitted by the user, call makeGrid()
-document.querySelector('input[type=submit]').addEventListener('click', makeGrid(canvasWidth, canvasHeight));
+GridSizeSubmitButton.addEventListener('click', function () {
+	canvasWidth = document.getElementById('inputWidth');
+	console.log("canvasWidth", canvasWidth);
+	canvasHeight = document.getElementById('inputHeight');
+	console.log("canvasHeight", canvasHeight);
 
-function makeGrid(width, height) {
+	// makeGrid(canvasWidth, canvasHeight);
+});
 
+// function makeGrid(width, height) {
 	//setup variables
 
-	var tableWidth = width.getAttribute('value');
-	var tableHeight = height.getAttribute('value');				
+	// var tableWidth = width.getAttribute('value');
+	
+	// var tableHeight = height.getAttribute('value');		
+		
 
 	//setup the table
-	for (i = 1; i <= tableHeight; i++) {
+	// for (i = 1; i <= tableHeight; i++) {
 		
-		tableRow = document.createElement('tr');
+	// 	tableRow = document.createElement('tr');
 
-		for (c = 1; c <= tableWidth; c++) {
-			tableCell = document.createElement('td');
-			tableRow.appendChild(tableCell);
-			// tableCell.innerHTML = i * tableWidth + c + 1;
-		}
+	// 	for (c = 1; c <= tableWidth; c++) {
+	// 		tableCell = document.createElement('td');
+	// 		tableRow.appendChild(tableCell);
+	// 	}
 
-		tableElement.appendChild(tableRow);
-	}
-}
+	// 	tableElement.appendChild(tableRow);
+	// }
+// }
