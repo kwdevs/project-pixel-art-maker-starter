b9a0e783c70ac5030673e19fc1c6011323c64880
Kyle Morrison <morrison.devs@gmail.com>
2018-02-05 14:59:16 -0600

    feat: fixed DOM selection value in initial variables.

--- designs.js
+++ designs.js
@@ -1,9 +1,9 @@
 // Select color input
 // Select size input
 
-var colorInput = document.getElementById('#colorPicker');
-var canvasHeight = document.getElementById('#inputHeight');
-var canvasWidth = document.getElementById('#inputWidth');
+var colorInput = document.getElementById('colorPicker');
+var canvasHeight = document.getElementById('inputHeight');
+var canvasWidth = document.getElementById('inputWidth');
 
 // When size is submitted by the user, call makeGrid()
 
