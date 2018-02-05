4c6dc8111977a3f76c07cd9aa90c13d53121f201
Kyle Morrison <morrison.devs@gmail.com>
2018-02-05 16:31:38 -0600

    feat: working on makeGrid function logic, still not working correctly.

--- designs.js
+++ designs.js
@@ -4,6 +4,7 @@
 var colorInput = document.getElementById('colorPicker');
 var canvasHeight = document.getElementById('inputHeight');
 var canvasWidth = document.getElementById('inputWidth');
+var tableElement = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
 document.querySelector('input[type=submit]').addEventListener('click', makeGrid(canvasWidth, canvasHeight));
@@ -12,15 +13,20 @@ function makeGrid(width, height) {
 
 	//setup variables
 
-	var tableElement = document.getElementById('pixelCanvas');
-
 	var tableWidth = width.getAttribute('value');
 	var tableHeight = height.getAttribute('value');				
 
 	//setup the table
-	for (i = 0; i <= tableHeight; i++) {
+	for (i = 1; i <= tableHeight; i++) {
 		
-			tableElement.insertAdjacentHTML('afterbegin', '<tr></tr>');
+		tableRow = document.createElement('tr');
 
+		for (c = 1; c <= tableWidth; c++) {
+			tableCell = document.createElement('td');
+			tableRow.appendChild(tableCell);
+			// tableCell.innerHTML = i * tableWidth + c + 1;
+		}
+
+		tableElement.appendChild(tableRow);
 	}
 }
