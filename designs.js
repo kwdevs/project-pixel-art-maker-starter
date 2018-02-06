f4e51db270acfe38f3eb4f9df99523d36c630d44
Kyle Morrison <morrison.devs@gmail.com>
2018-02-05 18:30:52 -0600

    fix: returned designs.js to original state.

--- designs.js
+++ designs.js
@@ -1,9 +1,6 @@
 // Select color input
 // Select size input
 
-var colorInput = document.querySelector('#colorPicker');
-var canvasHeight = document.querySelector('#inputHeight');
-var canvasWidth = document.querySelector('#inputWidth');
 
 // When size is submitted by the user, call makeGrid()
 
