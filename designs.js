8d70f81d9faa4ab2a9cbc1940bea7e985c415a63
Kyle Morrison <morrison.devs@gmail.com>
2018-02-06 12:44:45 -0600

    feat: Psuedocode and variable instantiations/definitions.

--- designs.js
+++ designs.js
@@ -1,11 +1,34 @@
-// Select color input
-// Select size input
+// Declare variable gridSizeInput as an array which stores user input of height and width. Declare currentColor to hold a string value.
 
+var gridSizeInput = [];
+var currentColor = '';
 
-// When size is submitted by the user, call makeGrid()
+// Define variable tableElement to hold table DOM element
 
-function makeGrid() {
+var tableElement = document.getElementById('pixelCanvas');
 
-// Your code goes here!
+// Define variable sizeSubmitButton to hold submit button in size form
+
+var sizeSubmitButton = document.getElementById('sizePicker'[type=submit]);
+
+// Listen for form #sizePicker submit button to be clicked. When clicked, run function to update gridSizeInput array and call makeGrid function.
+
+ 
+// makeGrid accepts 2 parameters, heigh and width, derived from user input stored in gridSizeInput array.
+
+function makeGrid(height, width) {
+
+// Define variable tableRowHTML as a tr html string
+
+// Define tableColHTML as a td html string.
+
+// Define canvasHTML as value of #pixelCanvas selection.
+
+// Check if creating first row
+
+// Create and attach gridSizeInput[0], rows value, to canvasHTML.
+// If first row, attach adjacent as first, else attach adjacent as
+// last element.
+
 
 }
